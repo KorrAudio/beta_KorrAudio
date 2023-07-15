00cf661d4fc016c5c8f0c85d3d5264a4ea94f1dc
KorrAudio <139574456+KorrAudio@users.noreply.github.com>
2023-07-15 18:02:25 +0200

    Update KorrAudio.py
    undo

--- KorrAudio.py
+++ KorrAudio.py
@@ -169,7 +169,6 @@ layout = [
                     [sg.Button(plot_title) for plot_title, _ in plots]
                 ]),
                 sg.Tab('Help', [
-                    [sg.Text("")],
                     [sg.Text("Supported Audio Formats:")],
                     [sg.Text(", ".join(SUPPORTED_FORMATS))],
                     [sg.Text("")],
